6c6d49694ade2ab93565986ac1103934d466301e
Umar <umarnaveed001@gmail.com>
2022-12-31 19:14:39 +0500

    first commit

---
+++ pages/api/hello-edge.js
@@ -0,0 +1,18 @@
+
+export const config = {
+    runtime: 'edge',
+}
+
+export default async function handler(req) {
+    return new Response(
+        JSON.stringify({
+            name: 'Jim Halpert',
+        }),
+        {
+            status: 200,
+            headers: {
+                'content-type': 'application/json',
+            },
+        }
+    )
+}
